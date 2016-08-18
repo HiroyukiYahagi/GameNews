4a9051aa9ed402e475117ea502fd7f507e846bf8
HiroyukiYahagi <yahagi1989@gmail.com>
2016-08-18 23:30:02 +0900

    Initial Commit

---
+++ GameNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameNews
+//
+//  Created by Hiroyuki Yahagi on 2016/08/18.
+//  Copyright © 2016年 Hiroyuki Yahagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
